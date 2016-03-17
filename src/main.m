2bacd2ca4b2b426f62bc7ecee9985b9c05fd75f6
khushhallchandra <kcm00000@gmail.com>
2016-03-17 23:57:15 +0530

    Started

---
+++ src/main.m
@@ -0,0 +1,13 @@
+%Main
+tic;
+
+%Read the database directory
+readdir = uigetdir('','Choose Directory for reading database');
+
+ind=0;
+G_bandlimited_poc=zeros(1,20);
+G_poc=zeros(1,20);
+
+
+
+toc;
